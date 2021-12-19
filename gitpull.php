779cb378632630c57bf53063df946eb871926c3c
AayushB2 <93049736+AayushB2@users.noreply.github.com>
2021-12-18 22:00:03 -0500

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -1,1 +1,9 @@
-exec(git pull);
+?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+shell_exec( ‘C:\FTC\21-22\FtcRobotController-master && git reset –hard HEAD && git pull’ );
+}
+
+?>hi
