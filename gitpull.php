ed373a75d9f5c2f0e7d1912e05f7439a7106cb2e
AayushB2 <93049736+AayushB2@users.noreply.github.com>
2022-04-15 11:05:24 -0400

    Delete gitpull.php

--- gitpull.php
+++
@@ -1,9 +0,0 @@
-?php
-
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ( $_POST['https://github.com/AayushB2/test/blob/master/gitpull.php'] ) {
-shell_exec( ‘C:\FTC\21-22\FtcRobotController-master && git reset –hard HEAD && git pull’ );
-}
-
-?>hi
